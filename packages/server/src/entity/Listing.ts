5857007b1396379b2c61e8654833b08713110094
Ridho <muhammadridhoputra@gmail.com>
2020-09-24 13:48:00 +0700

    create listing resolver

--- packages/server/src/entity/Listing.ts
+++ packages/server/src/entity/Listing.ts
@@ -1,41 +1,42 @@
-import * as bcrypt from "bcryptjs";
 import {
   Entity,
   Column,
   BaseEntity,
   PrimaryGeneratedColumn,
-  BeforeInsert,
+  ManyToOne,
 } from "typeorm";
+import { User } from "./User";
 
 @Entity("listings")
 export class Listing extends BaseEntity {
-  @PrimaryGeneratedColumn("uuid")
-  id: string;
+  @PrimaryGeneratedColumn("uuid") id: string;
 
   @Column("varchar", { length: 100 })
   name: string;
 
-  @Column("text")
-  pictureUrl: string;
+  @Column("varchar", { length: 100 })
+  category: string;
+
+  @Column("text") pictureUrl: string;
 
   @Column("varchar", { length: 255 })
   description: string;
 
-  @Column("int")
-  price: number;
+  @Column("int") price: number;
 
-  @Column("double precision")
-  latitude: number;
+  @Column("int") beds: number;
 
-  @Column("double precision")
-  longitude: number;
+  @Column("int") guests: number;
 
-  @Column("int")
-  beds: number;
+  @Column("double precision") latitude: number;
 
-  @Column("int")
-  guests: number;
+  @Column("double precision") longitude: number;
 
   @Column("text", { array: true })
   amenities: string[];
+
+  @Column("uuid") userId: string;
+
+  @ManyToOne(() => User, (user) => user.listings)
+  user: User;
 }
